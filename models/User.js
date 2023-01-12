99aeeb7b59f06d2c7a41f66f33093ba52aa51c7f
chengu <jeddycheng@gmail.com>
2023-01-12 09:05:28 -0800

    foundation set

--- models/User.js
+++ models/User.js
@@ -9,11 +9,7 @@ const userSchema = new mongoose.Schema({
         type: String, 
         required:[true, "Password is Required!"],
     },
-},
-    {
-        timestamps: true
-    
-    });
+});
 
 const User = mongoose.model("User", userSchema)
 
---
+++ server.js
@@ -0,0 +1,60 @@
+require("./config/db.connection")
+
+require("dotenv").config()
+
+// pull PORT from .env, give default value of 4000 and establish DB Connection
+const { PORT, MONGODB_URI } = process.env;
+
+// importing
+const morgan = require("morgan");
+
+const cors = require("cors");
+
+const express = require("express");
+
+const app = express();
+
+// MIDDLEWARE
+app.use(express.json());
+
+app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString(); 
+  next();
+});
+
+// Controllers 
+
+
+
+
+// Api Routes
+
+app.use("/chat", chatController);
+app.use((err, req, res, next) => res.status(500).send(err));
+
+app.use("/user", userController);
+app.use((err, req, res, next) => res.status(500).send(err));
+
+
+
+
+
+app.all("/*", function (req, res) {
+    return res.status(404).json({ errata: "No Resource Found Genius!" });
+  });
+  
+  app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
